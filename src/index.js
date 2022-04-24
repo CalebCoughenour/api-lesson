059b3acd2c4581c343dfb73eed7d6af0ba26c778
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-04-24 13:45:54 -0700

    update with gif search

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import WeatherService from './js/weather-service.js';
+import WeatherService from './services/weather-service.js';
+import GiphyService from './services/giphy-service.js';
 
 function clearFields() {
   $('#location').val("");
@@ -11,25 +12,45 @@ function clearFields() {
   $('.showTemp').text("");
 }
 
-function getElements(response) {
-  if (response.main) {
-    $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
-    $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
-    $('.showWind').text(`Current wind speed is ${response.wind.speed} m/s & direction ${response.wind.deg} degrees.`);
-    $('.showDescription').text(response.weather[0].description);
-  } else {
-    $('.showErrors').text(`There was an error: ${response}`);
-  }
-}
+const displayGif = (response) => {
+  const url = response.data[0].images.downsized.url;
+  $('.showGif').html(`<img src='${url}'>`);
+};
+
+const displayWeatherDescription = (description) => {
+  $('.showDescription').text(`Weather Description: ${description}`);
+};
+
+const displayErrors = (error) => {
+  $('.show-errors').text(`${error}`);
+};
+
 
 $('#weatherLocation').click(function() {
   let zipcode = $('#location').val();
   let language = $('#language-picker').val();
   clearFields();
-  (async function() {
-    const response = await WeatherService.getWeather(zipcode, language);
-    getElements(response);
-  })();
+  WeatherService.getWeather(zipcode, language)
+    .then(function(weatherResponse) {
+      if (weatherResponse instanceof Error) {
+        throw Error(`OpenWeather API Error: ${weatherResponse.message}`);
+    }
+    const weatherDescription = weatherResponse.weather[0].description;
+    displayWeatherDescription(weatherDescription);
+    $('.showHumidity').text(`The humidity in ${weatherResponse.name} is ${weatherResponse.main.humidity}%`);
+    $('.showTemp').text(`The temperature in Fahrenheit is ${weatherResponse.main.temp} degrees.`);
+    $('.showWind').text(`Current wind speed is ${weatherResponse.wind.speed} m/s & direction ${weatherResponse.wind.deg} degrees.`);
+    return GiphyService.getGif(weatherDescription);
+    })
+    .then(function(giphyResponse) {
+      if (giphyResponse instanceof Error) {
+        throw Error(`Giphy API Error: ${giphyResponse.message}`);
+      }
+      displayGif(giphyResponse);
+    })
+    .catch(function(error) {
+      displayErrors(error.message);
+    });
 });   
 
 
--- src/services/weather-service.js
+++ src/services/weather-service.js
@@ -1,7 +1,7 @@
 export default class WeatherService {  
   static async getWeather(zipcode, language) {
     try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${process.env.API_KEY}&units=imperial&lang=${language}`);
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=imperial&lang=${language}`);
         if (!response.ok) {
           throw Error(response.statusText);
         }
